621103ce1eef8938709272f201f17d1e96c187ac
Ansh2030 <anshsrivastava360@gmail.com>
2022-10-03 17:07:30 +0530

    Updates

---
+++ precache-manifest.f641a9afe8d734431888027f8483ab47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b5aa0e3c3942db541d6",
+    "url": "/txtutil_1/static/css/main.7b73c23b.chunk.css"
+  },
+  {
+    "revision": "4b5aa0e3c3942db541d6",
+    "url": "/txtutil_1/static/js/main.4b5aa0e3.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/txtutil_1/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/txtutil_1/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "1f6eea5017171f39d165",
+    "url": "/txtutil_1/static/js/runtime~main.1f6eea50.js"
+  },
+  {
+    "revision": "8b8b566ad9e212bd91becc44ad54e1e3",
+    "url": "/txtutil_1/index.html"
+  }
+];
